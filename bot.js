1a2428744a54e1678674ec60853151a12b40d3e0
Monji69 <48274432+Monji69@users.noreply.github.com>
2019-04-07 13:54:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`احتاج سرفر انفايت `,"http://twitch.tv/Dream")
+client.user.setGame(`ارسلي سرفر في خاص عادي `,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
