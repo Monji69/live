217386985924648a3d096f055f0b650fff8e70e3
Monji69 <48274432+Monji69@users.noreply.github.com>
2019-04-07 15:16:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`اعتزال | انا بوت الان `,"http://twitch.tv/Dream")
+client.user.setGame(`اعتزال | بوت اعتزال `,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
